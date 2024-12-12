311529d54c46bed9f4e0c4e603b258e076515897
MathieuE-dev <mathieu.exposito@outlook.fr>
2024-12-12 20:32:59 +0100

    Delete sujet2/restaurant-reservation directory

--- sujet2/restaurant-reservation/app/components/HeroSection.tsx
+++
@@ -1,7 +0,0 @@
-export default function HeroSection() {
-    return (
-      <div className="bg-cover bg-center h-96 text-white flex items-center justify-center" style={{ backgroundImage: "url('/restaurant.jpg')" }}>
-        <h1 className="text-5xl font-bold">Bienvenue dans notre restaurant</h1>
-      </div>
-    );
-  }
